633392d0aae1c81b7c51ba10955c88d4edae6e63
DESKTOP-6TLLA01\Ez <sunduoze@163.com>
2021-08-18 19:31:33 +0800

    commit@20210818 + aslan product test func.

--- main.py
+++ main.py
@@ -234,6 +234,107 @@ def get_status(dev: c_void_p):
     return ret, error_info
 
 
+
+def addr_write(dev_addr, reg_addr, reg_addr_len, data, data_len):
+    time_out = 100
+    # write request, need add cancel tansfer ?
+    dev_addr = c_byte(dev_addr)
+    buffer_array = c_byte * 64
+    if reg_addr_len != 0:
+        buffer = buffer_array(reg_addr)
+        n_bytes = c_byte(reg_addr_len)
+        ret, error_info = cp2112.HidSmbus_WriteRequest(dev, dev_addr, pointer(buffer), n_bytes)
+        # print("5 error info:", ret, error_info)
+        time.sleep(time_out * 0.001)
+        # ret, error_info = cp2112.HidSmbus_CancelTransfer(dev)
+
+    buffer = data # buffer_array(0x03, 0x84, 0x01, 0x4B, 0x0D)
+    n_bytes = c_byte(data_len) # c_byte(5)
+    ret, error_info = cp2112.HidSmbus_WriteRequest(dev, dev_addr, pointer(buffer), n_bytes)
+    # print("5 error info:", ret, error_info)
+    time.sleep(time_out * 0.001)
+
+    return ret
+
+def aslan_pack_beacon_config():
+    buffer_array = c_byte * 64
+
+
+# Beacon data
+    buf = buffer_array(0x0A,
+                       0x1F, 0x86, 0x02, 0x1B, 0xFF, 0x01, 0xF1, 0xBE,
+                       0xAC, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77,
+                       0x88, 0x99, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF,
+                       0x1A, 0xEB, 0xEB, 0xEC, 0xDD, 0xC3, 0x00, 0x1A,
+                       0x0D)
+    addr_write(0x42, 0x0A, 0, buf, 34)
+    time.sleep(0.1)
+
+# Tx power set
+    buf = buffer_array(0x0A,
+                       0x03, 0x82, 0x01, 0x4B, 0x0D)
+    addr_write(0x42, 0x0A, 0, buf, 6)
+    time.sleep(0.1)
+
+# Tx rate set
+    buf = buffer_array(0x0A,
+                       0x04, 0x83, 0x00, 0x01, 0x65, 0x0D)
+    addr_write(0x42, 0x0A, 0, buf, 7)
+    time.sleep(0.1)
+
+# # beacon enable
+#     buf = buffer_array(0x0A,
+#                        0x03, 0x84, 0x01, 0x4B, 0x0D)
+#     addr_write(0x42, 0x0A, 0, buf, 6)
+#     time.sleep(0.1)
+
+# # beacon disable
+#     buf = buffer_array(0x0A,
+#                        0x03, 0x84, 0x00, 0x02, 0x0D)
+#     addr_write(0x42, 0x0A, 0, buf, 6)
+
+def aslan_pack_beacon_status(status):
+    if(status is True):
+        buffer_array = c_byte * 64
+        # beacon enable
+        buf = buffer_array(0x0A,
+                           0x03, 0x84, 0x01, 0x4B, 0x0D)
+        addr_write(0x42, 0x0A, 0, buf, 6)
+        time.sleep(0.1)
+        print("beacon is opened")
+    else:
+        buffer_array = c_byte * 64
+        # beacon disable
+        buf = buffer_array(0x0A,
+                           0x03, 0x84, 0x00, 0x02, 0x0D)
+        addr_write(0x42, 0x0A, 0, buf, 6)
+        print("beacon is closed")
+
+def aslan_pack_beacon_thermal_pin(status):
+    # Set all GPIO to OUTPUT
+    #0xF0: GPIO 0:3 ->input 4:7 ->output
+    ret, error_info = cp2112.HidSmbus_SetGpioConfig(dev, c_byte(0xF0), c_byte(0x00), c_byte(0x00), c_byte(0x00))
+    # print(ret, error_info)
+    if(status is True):
+        # 0xF0: GPIO 0:3 ->low 4:4 -> high 5:7 ->low
+        ret = cp2112.HidSmbus_WriteLatch(dev, c_byte(0x10), c_byte(0xFF))
+        print("pin pull high, i2c cmd mode")
+    else:
+        ret = cp2112.HidSmbus_WriteLatch(dev, c_byte(0x00), c_byte(0xFF))
+        print("default(internel pull down 100k), beacon boardcast mode")
+    return ret, error_info
+
+
+
+# dev_addr:21
+# reg_addr:0A
+#
+# beacon enable W:03 84 01 4B 0D
+# Tx rate set   W:04 83 00 01 65 0D
+# Tx power set  W:03 82 01 0B 0D
+# Beacon data   W:1F 86 02 1B FF 01 F1 BE AC 11 22 33 44 55 66 77 88 99 AA BB CC DD EE FF 1A EB EB EC DD C3 00 1A 0D
+
+
 if __name__ == '__main__':
 
     # 1. judge cp2112 dev status & get num Devices
@@ -258,44 +359,61 @@ if __name__ == '__main__':
 
     # 3. set smbus configuration
     time_out = 100  # ms
-    set_smbus_config(dev, c_ulong(100000), c_ushort(time_out), c_ushort(3))
+    set_smbus_config(dev, c_ulong(33000), c_ushort(time_out), c_ushort(3))
 
-    # 4. write request, need add cancel tansfer ?
-    dev_addr = c_byte(0x16)
-    buffer_array = c_byte * 16
-    buffer = buffer_array(0x80, 0xEE, 0x03, 0x84)
-    n_bytes = c_byte(4)
-    ret, error_info = cp2112.HidSmbus_WriteRequest(dev, dev_addr, pointer(buffer), n_bytes)
-    # print("5 error info:", ret, error_info)
-    time.sleep(time_out * 0.001)
-    # ret, error_info = cp2112.HidSmbus_CancelTransfer(dev)
+    time.sleep(0.1)
 
-    # write other data
-    buffer = buffer_array(0x81, 0xFF, 0x03, 0xAD)
-    n_bytes = c_byte(4)
-    ret, error_info = cp2112.HidSmbus_WriteRequest(dev, dev_addr, pointer(buffer), n_bytes)
-    time.sleep(time_out * 0.001)
+    aslan_pack_beacon_thermal_pin(True)
+    time.sleep(1)
+    aslan_pack_beacon_config()
+    time.sleep(1)
 
-    get_status(dev)
 
-    # ret, error_info = cp2112.HidSmbus_CancelTransfer(dev)
-    # print("6 error info:", ret, error_info)
+#### enable
+    aslan_pack_beacon_status(True)
+#### disable
+    aslan_pack_beacon_status(False)
 
-    # 5. reg addr address read
-    # BM191 test ADDR_W:0x16 W:0x80 ADDR_R:0x16 R:0xEE 0x03 0x60(crc8)
-    byte_array = c_byte * 16
-    reg_addr_buf = byte_array(0x81)
-    reg_addr_read(dev, c_byte(0x16), c_byte(1), reg_addr_buf, c_ushort(3))
+    time.sleep(1)
+    aslan_pack_beacon_thermal_pin(False)
+    time.sleep(6)
 
-    # 6. smbus read function
-    # read(dev, c_byte(0x16), c_ushort(3))
-
+    # # 4. write request, need add cancel tansfer ?
+    # dev_addr = c_byte(0x16)
+    # buffer_array = c_byte * 16
+    # buffer = buffer_array(0x80, 0xEE, 0x03, 0x84)
+    # n_bytes = c_byte(4)
+    # ret, error_info = cp2112.HidSmbus_WriteRequest(dev, dev_addr, pointer(buffer), n_bytes)
+    # # print("5 error info:", ret, error_info)
+    # time.sleep(time_out * 0.001)
+    # # ret, error_info = cp2112.HidSmbus_CancelTransfer(dev)
+    #
+    # # write other data
+    # buffer = buffer_array(0x81, 0xFF, 0x03, 0xAD)
+    # n_bytes = c_byte(4)
+    # ret, error_info = cp2112.HidSmbus_WriteRequest(dev, dev_addr, pointer(buffer), n_bytes)
+    # time.sleep(time_out * 0.001)
+    #
     # get_status(dev)
-
-    # 6. test cp2112 gpio
-    cp2112_gpio_test(dev)
-
-    get_status(dev)
-
-    # 8. get part number & version
-    cp2112_get_lib_version(dev)
+    #
+    # # ret, error_info = cp2112.HidSmbus_CancelTransfer(dev)
+    # # print("6 error info:", ret, error_info)
+    #
+    # # 5. reg addr address read
+    # # BM191 test ADDR_W:0x16 W:0x80 ADDR_R:0x16 R:0xEE 0x03 0x60(crc8)
+    # byte_array = c_byte * 16
+    # reg_addr_buf = byte_array(0x81)
+    # reg_addr_read(dev, c_byte(0x16), c_byte(1), reg_addr_buf, c_ushort(3))
+    #
+    # # 6. smbus read function
+    # # read(dev, c_byte(0x16), c_ushort(3))
+    #
+    # # get_status(dev)
+    #
+    # # 6. test cp2112 gpio
+    # cp2112_gpio_test(dev)
+    #
+    # get_status(dev)
+    #
+    # # 8. get part number & version
+    # cp2112_get_lib_version(dev)
